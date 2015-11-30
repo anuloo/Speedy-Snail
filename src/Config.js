6356ab9529e4c29e1ab83a0c71a08648fc95f35f
anuloo <jollzy@gmail.com>
2015-11-30 23:57:49 +0000

    asset loading

---
+++ src/Config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jollzy on 30/11/2015.
+ * Keep all the positions other constant values and event types in one place.
+ */
+var Constants={
+    GAME_WIDTH : 1136,
+    GAME_HEIGHT : 1136
+};
+var GameEventType={
+    ASSETS_LOADED : "GameEventType.ASSETS_LOADED"
+};
---
+++ src/EventDispatcher.js
@@ -0,0 +1,3 @@
+/**
+ * Created by jollzy on 30/11/2015.
+ */
